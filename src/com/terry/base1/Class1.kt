0f99cb9d6139bee7ebf5b558951c8906eeb76e6f
terry <fangxianchao@zhehekeji.com>
2022-04-07 22:12:07 +0800

    变量声明

---
+++ src/com/terry/base1/Class1.kt
@@ -0,0 +1,25 @@
+package com.terry.base1
+
+
+fun main() {
+
+
+    //变量声明：var 可读可改   val:只读   默认会进行数据类型推导
+    var nameR = "terry"
+    val nameL = "terry-val"
+
+
+    /**
+     * Kotlin  中的内置数据类型：
+     *     1.String  字符串
+     *     2.Char   单字符
+     *     3.Boolean true/false
+     *     4.Int  整形
+     *     5.Double 小数
+     *     6.List 集合
+     *     7.Set 无重复的元素集合
+     *     8.Map 键值对的集合
+     * **/
+
+
+}
